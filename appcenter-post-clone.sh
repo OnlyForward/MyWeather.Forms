c859f4df8cb16cedbf57dd178cf39d5c12ff12b5
DimaSh <shibanov-1997@inbox.ru>
2019-07-30 02:51:12 -0700

    Update and rename post-clone to appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,3 +2,4 @@
 
 wget https://dl.xamarin.com/MonoFrameworkMDK/Macx86/MonoFramework-MDK-6.0.0.311.macos10.xamarin.universal.pkg
 sudo installer -pkg "MonoFramework-MDK-6.0.0.311.macos10.xamarin.universal.pkg" -target /
+sudo ln -s /Library/Frameworks/Mono.framework/Versions/6.0.0 /Library/Frameworks/Mono.framework/Versions/5_18_2
