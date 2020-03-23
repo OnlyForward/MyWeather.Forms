c34c6e1ccba85e26d56ccb94ca47837bccbfb2ca
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-03-23 12:43:39 +0300

    fix

--- MyWeather.Droid/appcenter-pre-build.sh
+++ MyWeather.Droid/appcenter-pre-build.sh
@@ -4,4 +4,4 @@ java -version
 export JAVA_HOME=$(/usr/libexec/java_home -v11)
 echo "changed java version to 1.11"
 java -version
-echo "export JAVA_HOME=$(/usr/libexec/java_home -v1.11)" >> "${HOME}/.bashrc"
+echo "export JAVA_HOME=$(/usr/libexec/java_home -v11)" >> "${HOME}/.bashrc"
