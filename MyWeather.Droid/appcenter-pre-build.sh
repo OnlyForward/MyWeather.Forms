fd41d8489042b61b5af4fce8d8eb0d4fc7a48c56
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-03-24 10:51:21 +0300

    added post clone script

--- MyWeather.Droid/appcenter-pre-build.sh
+++ MyWeather.Droid/appcenter-pre-build.sh
@@ -1,7 +1,2 @@
-#!/usr/bin/env bash
-echo "defaul java version is"
+echo "java version in prebuild step"
 java -version
-export JAVA_HOME=$(/usr/libexec/java_home -v11)
-echo "changed java version to 1.11"
-java -version
-echo "export JAVA_HOME=$(/usr/libexec/java_home -v11)" >> "${HOME}/.bashrc"
